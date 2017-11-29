60646946e97638142b6fe2552d6e086eb6e304b7
nmassey001 <neil.massey@stfc.ac.uk>
2017-11-29 14:25:30 +0100

    Added function get_source_target_slices

--- S3netCDF4/_CFAFunctions.pyx
+++ S3netCDF4/_CFAFunctions.pyx
@@ -318,3 +318,42 @@ def fill_slices(master_array_shape, elems):
     for s in range(0, fill_number):
         slices.append(slice(0, master_array_shape[start_number+s]-1, 1))
     return slices
+
+
+def get_source_target_slices(partition, elem_slices):
+    """Get the slice into the source subarray and the target slice for the destination subarray,
+       based on the information in the partition and the subdomain of the master array defined
+       by elem_slices.  elem_slices are the filled slices from the function above."""
+
+    # create the default source slice: this is the shape of the subarray
+    source_slice = []
+    for sl in partition.subarray.shape:
+        source_slice.append(CFASlice(0, sl, 1))
+    # create the target slice from the location and the passed in elements
+    # create the default slice first - we will modify this
+    target_slice = []
+    for pl in partition.location:
+        target_slice.append(CFASlice(pl[0], pl[1], 1))
+
+    # now modify the slice based on the elements passed in
+    py_source_slice = []
+    py_target_slice = []
+    for p in range(0, len(target_slice)):
+        # adjust the end target_slice if we are taking a subset of the data
+        if elem_slices[p].stop < target_slice[p].stop:
+            source_slice[p].stop -= target_slice[p].stop - elem_slices[p].stop
+            target_slice[p].stop = elem_slices[p].stop
+        # adjust the target start and end for the sub_slice
+        target_slice[p].start -= elem_slices[p].start
+        target_slice[p].stop -= elem_slices[p].start - 1
+        # check if the elem started within the location
+        if target_slice[p].start < 0:
+            # source_slice.start is absolute value of ts
+            source_slice[p].start = -1 * target_slice[p].start
+            # target start is 0
+            target_slice[p].start = 0
+
+        py_source_slice.append(source_slice[p].to_pyslice())
+        py_target_slice.append(target_slice[p].to_pyslice())
+
+    return py_source_slice, py_target_slice
