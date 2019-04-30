387a4357a2385d9cfa624cacb111c907bdca0e3f
nmassey001 <neil.massey@stfc.ac.uk>
2019-04-30 15:08:01 +0100

    Renamed exceptions

---
+++ S3netCDF4/Managers/_FileManager.pyx
@@ -0,0 +1,70 @@
+"""
+File management for S3netCDF.  Operation:
+o. Files are opened from a single URL
+o. The manager determines, using the ConfigManager, which fileobject to use to
+   open the file
+o. A file object is returned.
+o. Reading / writing to a file can then be performed by operations on the file
+   object.
+"""
+
+__copyright__ = "(C) 2019 Science and Technology Facilities Council"
+__license__ = "BSD - see LICENSE file in top-level directory"
+
+from S3netCDF4.Managers._ConfigManager import Config
+from S3netCDF4._Exceptions import *
+from S3netCDF4.Backends import *
+from urllib.parse import urlparse
+
+class FileManager(object):
+    """Class to return a file object handle when supplied with a URL / URI /
+    filepath.  Uses an instance of the ConfigManager to read in the mapping
+    between aliases and storage systems."""
+
+    """Static member variable: ConfigManager"""
+    _config = Config()
+
+    def open(self, url, mode="r"):
+        """Open a file on one of the supported backends and return a
+        corresponding fileobject to it."""
+
+        # split the url into the scheme, netloc, etc.
+        url_o = urlparse(url)
+        # the alias is the scheme + "://" + netloc
+        alias = url_o.scheme + "://" + url_o.netloc
+        # check if alias is in the config dictionary under "hosts"
+        if alias in FileManager._config["hosts"]:
+            # get which backend this url pertains to
+            try:
+                backend = FileManager._config["hosts"][alias]["backend"]
+                # get the true path
+                true_path = FileManager._config["hosts"][alias]["url"] + url_o.path
+                credentials = FileManager._config["hosts"][alias]["credentials"]
+            except:
+                raise APIException(
+                    "Configuration file for {} is incomplete: {}.".format(
+                        alias,
+                        FileManager._config["filename"]
+                    )
+                )
+            # create the argument string for creating the backend fileobject:
+            arg_str = (backend + '("' + true_path + '"' +
+                       ', mode="' + mode +
+                       '", credentials=' + str(credentials) +
+                       ')')
+            # use eval to execute the arg_str
+            fh = eval(arg_str)
+        else:
+            # try opening just on the file system
+            try:
+                fh = open(url)
+            except:
+                raise IOException(
+                    "URL or file {} is not found, or host {} is not present"
+                    " in the user config file: {}".format(
+                        url,
+                        alias,
+                        FileManager._config["filename"]
+                )
+            )
+        return fh
