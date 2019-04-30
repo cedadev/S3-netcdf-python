97b9c82fefd7c6afa079c9d1358b59addff88531
nmassey001 <neil.massey@stfc.ac.uk>
2019-04-30 15:06:45 +0100

    Added Cython language level

--- S3netCDF4/CFA/__init__.py
+++ S3netCDF4/CFA/__init__.py
@@ -120,7 +120,6 @@ import os
 os.environ["C_INCLUDE_PATH"] = np.get_include()
 pyximport.install(
     setup_args={'include_dirs': np.get_include()},
-    language_level=3,
-    )
+    language_level=2)
 
 #NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
