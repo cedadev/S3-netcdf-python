dd66f53eda5e9b8049773f7ab8614d8e3233b605
Neil Massey <neil.massey@stfc.ac.uk>
2020-10-01 16:43:38 +0100

    Corrected typo

--- S3netCDF4/CFA/_CFAClasses.pyx
+++ S3netCDF4/CFA/_CFAClasses.pyx
@@ -1171,7 +1171,6 @@ cdef class CFAVariable:
                     # whereas Python and CFA 0.5 have them as exclusive
                     tmp_loc = np.array(p["location"])
                     tmp_loc[:,1] += 1
-                    #tmp_loc[:,1] += 1
                     ncp.variables["location"][it] = tmp_loc
                     ncp.variables["ncvar"][it] = p["subarray"]["ncvar"]
                     ncp.variables["file"][it] = p["subarray"]["file"]
